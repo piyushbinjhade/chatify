4759a70d00b69b56d532343bce510e22e40ab567
piyushbinjhade <piyushbinjhade2019@gmail.com>
2025-12-05 03:01:58 +0530

    Add CORS middleware to enable cross-origin requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,12 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
+import cors from 'cors';
+
+app.use(cors({
+  origin: "https://chatify-1-pwpu.onrender.com", // your frontend URL
+  credentials: true
+}));
 
 const app = express()
 const __dirname = path.resolve();
@@ -15,16 +21,16 @@ app.use(express.json())  // req.body
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
-if (ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// // make ready for deployment
+// if (ENV.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    // Catch-all middleware for SPA routing (React Router)
-    app.use((req, res, next) => {
-        if (req.path.startsWith('/api')) return next(); // Skip API routes
-        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-    });
-}
+//     // Catch-all middleware for SPA routing (React Router)
+//     app.use((req, res, next) => {
+//         if (req.path.startsWith('/api')) return next(); // Skip API routes
+//         res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+//     });
+// }
 
 
 
