a97565e8f2f539a7bf674d2806f4c1331cbb1ebe
piyushbinjhade <piyushbinjhade2019@gmail.com>
2025-12-05 02:14:02 +0530

    Implement login and logout functionality

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { ENV } from "../lib/env.js";
 
+
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
 
@@ -68,4 +69,38 @@ export const signup = async (req, res) => {
     console.log("Error in signup controller:", error);
     res.status(500).json({ message: "Internal server error" });
   }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    } 
+    const isPasswordCorrect = await bcrypt.compare(password,user.password);
+    if(!isPasswordCorrect){
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    generateToken(user._id,res);
+    
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+
+  }
+  catch (error) {
+    console.error("Error in login controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = (_, res) => { 
+  res.cookie("jwt","",{ maxAge:0 });
+  res.status(200).json({ message: "Logged out successfully" });
 };
