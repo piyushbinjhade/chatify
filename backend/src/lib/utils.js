21449159e5cd26156321d1b390fd35fd1cae7cda
piyushbinjhade <piyushbinjhade2019@gmail.com>
2026-03-06 23:39:17 +0530

    refactor: update BASE_URL to use VITE_API_URL

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,7 +14,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true, // Accessible only by web server
-    sameSite: "strict", // CSRF protection
+    sameSite: NODE_ENV === "production" ? "none" : "strict",
     secure: NODE_ENV === "production",
   });
 
